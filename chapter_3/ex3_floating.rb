4e73eafba366cbf909f2492b980a6495a3b72345
Veronica Carnero <vcarnero@gmail.com>
2016-02-28 12:36:16 -0700

    Adding floats to exercies
    [#113796335]

---
+++ chapter_3/ex3_floating.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+puts " I will now count my chickens:"
+
+
+puts "Hens #{25.0 + 30 / 6}"
+
+puts "Roosters #{100.0 -25 *3 % 4}"
+
+puts "Now I will count the eggs:"
+
+
+puts 3.0 + 2.5 + 1.6 - 5.0 + 4 % 2 - 1 / 4 + 6.0
+
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+
+puts 3 + 2 < 5.0 - 7
+
+
+puts "What is 3 + 2? #{ 3.5 + 2.5 }"
+
+puts "What is 5 -7? #{5.0 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+
+puts "Is it greater? #{5.9 > -2}"
+
+puts "Is it greater or equal? #{5.0 >= -2}"
+
+puts "Is it less or equal? #{5.0 <= -2}"
