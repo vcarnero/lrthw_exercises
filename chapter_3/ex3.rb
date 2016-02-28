e554d404e1e9e81779b22178829c0e41e77f5ffb
Veronica Carnero <vcarnero@gmail.com>
2016-02-28 12:35:01 -0700

    skills drill for chapter 3
    [#113796335]

---
+++ chapter_3/ex3.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+puts " I will now count my chickens:"
+
+# counting the amount of hens & roosters using addition/subtraction/division & percent math symbols. Hens 25 plus five (thirty divided by six) .
+puts "Hens #{25 + 30 / 6}"
+# Rooster twenty five times 3, 75 modula 4, 100 - 3 = 97
+puts "Roosters #{100 -25 *3 % 4}"
+
+puts "Now I will count the eggs:"
+
+# four modula 2 and one divided by 4 are both zero. Add three, two , one, six minus 5.
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Is value less-than other value
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# 5 is not less-than -2
+puts 3 + 2 < 5 - 7
+
+# what is result three plus two
+puts "What is 3 + 2? #{ 3 + 2 }"
+# what is result five minus -7
+puts "What is 5 -7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# Is 5 greater than -2
+puts "Is it greater? #{5 > -2}"
+# Is 5 greater-than-equal to -2
+puts "Is it greater or equal? #{5 >= -2}"
+# Is 5 less-than-equal to -2
+puts "Is it less or equal? #{5 <= -2}"
+
