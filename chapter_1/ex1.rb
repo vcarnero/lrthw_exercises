941d3d0bdd2d40959129bd456cba89720f650963
Veronica Carnero <vcarnero@gmail.com>
2016-02-28 10:16:48 -0700

    Adding the study drills exercise.
    [#113796331]

--- chapter_1/ex1.rb
+++ chapter_1/ex1.rb
@@ -5,3 +5,6 @@ puts "This is fun."
 puts "Yay! Printing."
 puts "I'd must rather you 'not'."
 puts 'I "said" do not touch this'
+
+#Study drills, print another line. The hash/octothorpe character comments out a line
+puts "Script print another line"
