0d038f43e01331033b71c4ecf51073eb9e076afd
Veronica Carnero <vcarnero@gmail.com>
2016-02-29 14:13:34 -0700

    rubocop offenses fixed

--- chapter_1/ex1.rb
+++ chapter_1/ex1.rb
@@ -1,10 +1,11 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts "Yay! Printing."
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun.'
+puts 'Yay! Printing.'
 puts "I'd must rather you 'not'."
-puts 'I "said" do not touch this'
+puts 'I said do not touch this'
 
-#Study drills, print another line. The hash/octothorpe character comments out a line
-puts "Script print another line"
+# Study drills, print another line.
+# The hash/octothorpe character comments out a line
+puts 'Script print another line'
